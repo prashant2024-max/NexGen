f76480b09f6a016aeeddbb785744f71fbeb5da70
prashant2024-max <prashant.4fox@gmail.com>
2025-11-20 11:03:55 +0530

    .code updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,38 +1,26 @@
 $(document).ready(function () {
 
-  // AOS Initialize
-  AOS.init({
-    once: true,
-    mirror: false,
-    offset: 50,
-    duration: 800,
-    easing: "ease-in-out",
-  });
-
   // Sticky Header
   $(window).on("scroll", function () {
     $("header").toggleClass("stickyHead", $(this).scrollTop() > 30);
   });
 
-  // Hamburger menu toggle
+  // Hamburger Menu
   $(".menuIcon").on("click", function () {
     $(".menuCont").toggleClass("active");
     $("body").toggleClass("menu-open");
-
-    if (!$(".menu-overlay").length) {
-      $("header").append('<div class="menu-overlay"></div>');
-    }
+    $(".menu-overlay").toggleClass("active");
   });
 
   // Close menu
-  $(document).on("click", ".menuClose, .menu-overlay", function () {
+  $(document).on("click", ".menuClose, .menu-overlay.active", function () {
     $(".menuCont").removeClass("active");
     $("body").removeClass("menu-open");
-    $(".menu-overlay").remove();
+    $(".menu-overlay").removeClass("active");
   });
 
 
-  // Home Partners and Supporters Marquee
+  // Partners Marquee
   $(".heroLogoMarquee").slick({
     slidesToShow: 6,
     slidesToScroll: 1,
@@ -44,82 +32,75 @@ $(document).ready(function () {
     arrows: false,
     dots: false,
     pauseOnHover: false,
-    variableWidth: false,
     responsive: [
-      {
-        breakpoint: 1366,
-        settings: { slidesToShow: 5 },
-      },
-      {
-        breakpoint: 1280,
-        settings: { slidesToShow: 5 },
-      },
-      {
-        breakpoint: 850,
-        settings: { slidesToShow: 4 },
-      },
-      {
-        breakpoint: 601,
-        settings: { slidesToShow: 4 },
-      },
-      {
-        breakpoint: 500,
-        settings: { slidesToShow: 3 },
-      },
+      { breakpoint: 1366, settings: { slidesToShow: 5 } },
+      { breakpoint: 1280, settings: { slidesToShow: 5 } },
+      { breakpoint: 850,  settings: { slidesToShow: 4 } },
+      { breakpoint: 601,  settings: { slidesToShow: 4 } },
+      { breakpoint: 500,  settings: { slidesToShow: 3 } },
     ],
   });
 
 
 
-  // toggle dropdown
+  // Dropdown Toggle
   $(".selectPlaceholder").on("click", function (e) {
     e.stopPropagation();
+    $(".selectOptions").not($(this).next()).hide();
     $(this).next(".selectOptions").toggle();
   });
 
-  // select option
-  $(".selectOption").on("click", function () {
-    $(this).closest(".selectCountry").find(".selectPlaceholder").html($(this).html());
+  // Select Option
+  $(document).on("click", ".selectOption", function (e) {
+    e.stopPropagation();
+    $(this)
+      .closest(".selectCountry")
+      .find(".selectPlaceholder")
+      .html($(this).html());
     $(".selectOptions").hide();
   });
 
-  // click outside closes
+  // Click outside
   $(document).on("click", function () {
     $(".selectOptions").hide();
-  }); 
+  });
 
 
 
-
-
-  $(".psProductImg").slick({
-    dots: true,
+  // Product Slider
+  $(".aboutSliderBox").slick({
+    slidesToShow: 4.9,
+    slidesToScroll: 1,
+    dots: false,
     arrows: false,
     infinite: true,
     speed: 300,
     autoplay: true,
-    autoplaySpeed: 2000,
-    fade: true,
-    cssEase: "linear",
+    autoplaySpeed: 3000,
     pauseOnHover: false,
+    responsive: [
+      { breakpoint: 1366, settings: { slidesToShow: 4 } },
+      { breakpoint: 820,  settings: { slidesToShow: 3 } },
+      { breakpoint: 601,  settings: { slidesToShow: 2 } },
+      { breakpoint: 500,  settings: { slidesToShow: 1} },
+    ],
   });
 
-  // Tab Section
-  $(".tabBtn").click(function () {
-    var tab_id = $(this).data("id");
 
-    $(".tabBtn, .tab-content").removeClass("active");
-    $(this).addClass("active");
-    $("#" + tab_id).addClass("active");
-
-    // Reinitialize Slick slider inside the active tab
-    // $("#" + tab_id).find(".psAllArtBox").slick("setPosition");
+  // Product Slider 
+  $(".psPortSlider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 300000,
+    responsive: [
+      { breakpoint: 1440, settings: { slidesToShow: 2 } },
+      { breakpoint: 820,  settings: { slidesToShow: 2 } },
+      { breakpoint: 768,  settings: { slidesToShow: 1 } }
+    ],
   });
-
-  // Map hover and mobile click code
-  function isMobile() {
-    return window.matchMedia("(max-width: 1023px)").matches;
-  }
 
 });
-
